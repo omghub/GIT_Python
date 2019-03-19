b489a9436269adfea1e9c3a81fb3e7a16e9e7023
ATGamer8 <48680475+ATGamer8@users.noreply.github.com>
2019-03-19 10:34:05 +0700

    Upload file Python 10:33AM

--- Class_Lean/Array/Array/Array/Array.py
+++ Class_Lean/Array/Array/Array/Array.py
@@ -4,4 +4,5 @@ with open('AT.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     line_count = 0
     for row in csv_reader:
-       print(f'[Name {row[0]} | Pass:{row[1]}]')
+        dictionary = [row[0] , row[1]]
+        print(dictionary)
